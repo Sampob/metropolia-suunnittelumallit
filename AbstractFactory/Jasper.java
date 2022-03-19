89e9f46cd714ef1b54187b986ebc05f58e1357c8
Sampob <sampo.bredenberg@metropolia.fi>
2022-03-19 16:52:26 +0200

    Tehtävä 2

--- AbstractFactory/Jasper.java
+++ AbstractFactory/Jasper.java
@@ -5,22 +5,15 @@ public class Jasper {
     private Hat hat = null;
     private Shirt shirt = null;
     private Shoes shoes = null;
-    private ClothesFactory factory = null;
 
-    public void wearAdidas() {
-        factory = new AdidasFactory();
-    }
-
-    public void wearBoss() {
-        factory = new BossFactory();
-    }
-
-    public void showYourOutfit() {
+    public void wearOutfit(ClothesFactory factory) {
         pants = factory.createPants();
         hat = factory.createHat();
         shirt = factory.createShirt();
         shoes = factory.createShoes();
+    }
 
+    public void showYourOutfit() {
         System.out.println("I'm wearing: ");
         System.out.println(hat);
         System.out.println(shirt);
@@ -31,7 +24,10 @@ public class Jasper {
     public static void main(String[] args) {
 
         Jasper jasper = new Jasper();
-        jasper.wearBoss();
+        jasper.wearOutfit(new AdidasFactory());
+        jasper.showYourOutfit();
+
+        jasper.wearOutfit(new BossFactory());
         jasper.showYourOutfit();
     }
 }
